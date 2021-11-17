2d6a7e07335eab9860882227453b7ad3c5419bfd
jodufour <jodufour@student.42.fr>
2021-11-17 01:52:40 +0100

    Add colors.h to include/ [OK]

---
+++ include/colors.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 11:05:29 by jodufour          #+#    #+#             */
+/*   Updated: 2021/11/09 08:08:32 by jodufour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_H
+# define COLORS_H
+
+# define BLACK		"\e[30m"
+# define RED		"\e[31m"
+# define GREEN		"\e[32m"
+# define YELLOW		"\e[33m"
+# define BLUE		"\e[34m"
+# define MAGENTA	"\e[35m"
+# define CYAN		"\e[36m"
+# define WHITE		"\e[37m"
+# define RESET		"\e[0m"
+
+#endif
