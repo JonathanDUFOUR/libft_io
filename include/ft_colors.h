5e3582e669e9c835049bf37983c094d0c25bfee7
jodufour <jodufour@student.42.fr>
2022-10-29 06:47:58 +0000

    Update colors defines [OK]

--- include/ft_colors.h
+++ include/ft_colors.h
@@ -6,48 +6,33 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 11:05:29 by jodufour          #+#    #+#             */
-/*   Updated: 2022/03/12 04:27:02 by jodufour         ###   ########.fr       */
+/*   Updated: 2022/10/29 06:39:01 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_COLORS_H
 # define FT_COLORS_H
 
-# define BLACK		"\033[30m"
-# define RED		"\033[31m"
-# define GREEN		"\033[32m"
-# define YELLOW		"\033[33m"
-# define BLUE		"\033[34m"
-# define MAGENTA	"\033[35m"
-# define CYAN		"\033[36m"
-# define WHITE		"\033[37m"
+# define BLACK_FG	"\033[38;2;0;0;0m"
+# define RED_FG		"\033[38;2;255;0;0m"
+# define GREEN_FG	"\033[38;2;0;255;0m"
+# define BLUE_FG	"\033[38;2;0;0;255m"
+# define YELLOW_FG	"\033[38;2;255;255;0m"
+# define MAGENTA_FG	"\033[38;2;255;0;255m"
+# define CYAN_FG	"\033[38;2;0;255;255m"
+# define WHITE_FG	"\033[38;2;255;255;255m"
 
-# define BRIGHT_BLACK	"\033[30;1m"
-# define BRIGHT_RED		"\033[31;1m"
-# define BRIGHT_GREEN	"\033[32;1m"
-# define BRIGHT_YELLOW	"\033[33;1m"
-# define BRIGHT_BLUE	"\033[34;1m"
-# define BRIGHT_MAGENTA	"\033[35;1m"
-# define BRIGHT_CYAN	"\033[36;1m"
-# define BRIGHT_WHITE	"\033[37;1m"
+# define BLACK_BG	"\033[48;2;0;0;0m"
+# define RED_BG		"\033[48;2;255;0;0m"
+# define GREEN_BG	"\033[48;2;0;255;0m"
+# define BLUE_BG	"\033[48;2;0;0;255m"
+# define YELLOW_BG	"\033[48;2;255;255;0m"
+# define MAGENTA_BG	"\033[48;2;255;0;255m"
+# define CYAN_BG	"\033[48;2;0;255;255m"
+# define WHITE_BG	"\033[48;2;255;255;255m"
 
-# define BG_BLACK	"\033[40m"
-# define BG_RED		"\033[41m"
-# define BG_GREEN	"\033[42m"
-# define BG_YELLOW	"\033[43m"
-# define BG_BLUE	"\033[44m"
-# define BG_MAGENTA	"\033[45m"
-# define BG_CYAN	"\033[46m"
-# define BG_WHITE	"\033[47m"
-
-# define BG_BRIGHT_BLACK	"\033[40;1m"
-# define BG_BRIGHT_RED		"\033[41;1m"
-# define BG_BRIGHT_GREEN	"\033[42;1m"
-# define BG_BRIGHT_YELLOW	"\033[43;1m"
-# define BG_BRIGHT_BLUE		"\033[44;1m"
-# define BG_BRIGHT_MAGENTA	"\033[45;1m"
-# define BG_BRIGHT_CYAN		"\033[46;1m"
-# define BG_BRIGHT_WHITE	"\033[47;1m"
+# define BOLD		"\033[1m"
+# define BLINK		"\033[5m"
 
 # define RESET		"\033[0m"
 
